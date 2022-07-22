541093ccad8d7211596f531c2e4519160772fd78
Mircea Neacsu <mircea@neacsu.net>
2022-07-22 12:04:01 -0400

    Commenting

--- src/A.cpp
+++ src/A.cpp
@@ -1,4 +1,12 @@
+/*
+  This is part of an example showing how to manage multiple projects using
+  symbolic links and CPM - A C/C++ Package Manager. For more details
+  see: https://github.com/neacsum/cpm
+*/
+
+//how to include header files from this package
 #include <module_A/A.h>
+#include <module_B/B.h>
 
 int f_a (int i)
 {
--- src/B.cpp
+++ src/B.cpp
@@ -1,3 +1,9 @@
+/*
+  This is part of an example showing how to manage multiple projects using
+  symbolic links and CPM - A C/C++ Package Manager. For more details
+  see: https://github.com/neacsum/cpm
+*/
+
 #include <module_B/B.h>
 
 int f_2 (int i)
